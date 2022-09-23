bf8cbfa9436ec9ba06937186876c19875046d0aa
diadiawowka <v.kucinsky@gmail.com>
2022-09-23 16:35:51 +0300

    programist

---
+++ example_004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "андрей")
+{
+    Console.WriteLine("Программисту пулий!!!");
+}
+else
+{
+    Console.Write("Норок - Шнурок! ");
+    Console.WriteLine(username);
+}
